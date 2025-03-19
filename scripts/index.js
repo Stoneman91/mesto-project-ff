83c928f8b672f09eb9b6e6c4d57f7e3700fb71f2
Nikolay Boykov <nick.boickov@yandex.ru>
2025-03-19 21:04:10 +0300

    fix_rev1

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,33 @@
-//Создание карточки + заполнение данными
-function AddCard(cardData, removeCard) {
-    const cardTemplate = document.querySelector('#card-template').content;
+//Создание карточки + заполнение данными 
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsContainer = document.querySelector('.places__list');
+
+function createCard(cardData, removeCard) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     const cardImage = cardElement.querySelector('.card__image');
     const cardTitle = cardElement.querySelector('.card__title');
     cardImage.src = cardData.link;
     cardImage.alt = cardData.name;
     cardTitle.textContent = cardData.name;
-   //Обработчик удаления карточки
+
+    //Обработчик удаления карточки 
+
     const deleteButton = cardElement.querySelector('.card__delete-button');
     deleteButton.addEventListener('click', () => {
         removeCard(cardElement);
     });
     return cardElement;
-  }
-  // Удаление карточки
-  function deleteCard(cardElement) {
+}
+
+// Удаление карточки 
+
+function deleteCard(cardElement) {
     cardElement.remove();
-  }
-  // Вывод карточки
-  const placesList = document.querySelector('.places__list');
-  initialCards.forEach((cardData) => {
-    const cardElement = AddCard(cardData, deleteCard);
-    placesList.append(cardElement);
-  });
+}
+
+// Вывод карточки 
+
+initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData, deleteCard);
+    cardsContainer.append(cardElement);
+}); 
