5eefac10ef109712a9b921ab89e3a31936329020
Nikolay Boykov <nick.boickov@yandex.ru>
2025-04-24 21:52:03 +0300

    pr6

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,33 +1,35 @@
-//Создание карточки + заполнение данными 
-const cardTemplate = document.querySelector('#card-template').content;
-const cardsContainer = document.querySelector('.places__list');
+import '../pages/index.css';
+import { initialCards } from './cards.js';
 
-function createCard(cardData, removeCard) {
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    const cardImage = cardElement.querySelector('.card__image');
-    const cardTitle = cardElement.querySelector('.card__title');
-    cardImage.src = cardData.link;
-    cardImage.alt = cardData.name;
+const cardTemplate = document.querySelector('#card-template').content; 
+const cardsContainer = document.querySelector('.places__list'); 
+ 
+function createCard(cardData, removeCard) { 
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true); 
+    const cardImage = cardElement.querySelector('.card__image'); 
+    const cardTitle = cardElement.querySelector('.card__title'); 
+    cardImage.src = cardData.link; 
+    cardImage.alt = cardData.name; 
     cardTitle.textContent = cardData.name;
+ 
+    //Обработчик удаления карточки  
+ 
+    const deleteButton = cardElement.querySelector('.card__delete-button'); 
+    deleteButton.addEventListener('click', () => { 
+        removeCard(cardElement); 
+    }); 
+    return cardElement; 
+} 
+ 
+// Удаление карточки  
+ 
+function deleteCard(cardElement) { 
+    cardElement.remove(); 
+} 
+ 
+// Вывод карточки  
 
-    //Обработчик удаления карточки 
-
-    const deleteButton = cardElement.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', () => {
-        removeCard(cardElement);
-    });
-    return cardElement;
-}
-
-// Удаление карточки 
-
-function deleteCard(cardElement) {
-    cardElement.remove();
-}
-
-// Вывод карточки 
-
-initialCards.forEach((cardData) => {
-    const cardElement = createCard(cardData, deleteCard);
+initialCards.forEach((cardData) => { 
+    const cardElement = createCard(cardData, deleteCard); 
     cardsContainer.append(cardElement);
-}); 
+})
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
-    main: './src/index.js'
+    main: './src/scripts/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -26,8 +26,11 @@ module.exports = {
         exclude: '/node_modules/'
       },
       {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'
+        }
       },
       {
         test: /\.css$/,
