b9d69e33052231dd3c5dc64e01f0c052e5f5fdc9
Nikolay Boykov <nick.boickov@yandex.ru>
2025-04-27 22:02:09 +0300

    PR6_4_1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -30,3 +30,28 @@ addButton.addEventListener('click', () => {
   addForm.reset();
   openModal(popupAddCard);
 });
+
+
+// Находим форму в DOM
+const formElement = // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+const nameInput = // Воспользуйтесь инструментом .querySelector()
+const jobInput = // Воспользуйтесь инструментом .querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit); 
